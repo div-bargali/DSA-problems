d407b427c9ad93271d0b227d53fe562fbe619dfd
Divyanshu Bargali <divyanshubargali@gmail.com>
2023-12-19 23:14:57 +0530

    Time: 499 ms (25.27%), Space: 292.5 MB (21.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
-    int solve(int ind, int prev, int n, vector<int> &nums, vector<vector<int>> &dp) {
-        if (ind < 0) return 0;
-        if (dp[ind][prev] != -1) return dp[ind][prev];
-        if (prev == n || nums[ind] < nums[prev]) {
-            // have 2 choices -> either add nums[ind] to LIS
-            // or start a new LIS from nums[ind]
-            return dp[ind][prev] = max(1 + solve(ind-1, ind, n, nums, dp),
-                                       solve(ind-1, prev, n, nums, dp));
+    // Striver's approach // memoization + recursion
+    int solve(int ind, int prev, int n, vector<int>&nums, vector<vector<int>>&dp) {
+        if (ind == n) return 0;
+        // we are shifting prev by 1 to right because prev -> -1 -> n-1 and -1 can't be stored
+        // so prev -> 0 -> n
+        if (dp[ind][prev+1] != -1) return dp[ind][prev+1];
+        
+        if (prev == -1 || nums[ind] > nums[prev])
+            return dp[ind][prev+1] = max(1 + solve(ind+1, ind, n, nums, dp),
+                                      solve(ind+1, prev, n, nums, dp));
+        else 
+            return dp[ind][prev+1] = solve(ind+1, prev, n, nums, dp);
+    }
+    
+    int tabulation(int n, vector<int>& nums) {
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        // base case is already covered by initialization with 0
+        // reverse the loops for tabulation and copy recurence relation 
+        for(int ind = n-1; ind >= 0; ind--) {
+            for (int prev = ind-1; prev >= -1; prev--) {
+                if (prev == -1 || nums[ind] > nums[prev]) {
+                    dp[ind][prev+1] = max(1 + dp[ind+1][ind+1], 
+                                         dp[ind+1][prev+1]);
+                }
+                else
+                    dp[ind][prev+1] = dp[ind+1][prev+1];
+            }
         }
-        else {
-            // since not strictly increasing start a new LIS from nums[ind]
-            return dp[ind][prev] =  solve(ind-1, prev, n, nums, dp);
-        }
+        
+        return dp[0][0];
     }
     
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return solve(n-1, n, n, nums, dp);
+        // Approach 1. Memo + Recursion
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // return solve(0, -1, n, nums, dp);
+        
+        // Approach 2. tabulation
+        return tabulation(n, nums);
     }
 };
