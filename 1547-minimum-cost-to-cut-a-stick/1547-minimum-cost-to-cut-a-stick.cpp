fa7765e817188877192a2061c1825e482be2b28f
Divyanshu Bargali <divyanshubargali@gmail.com>
2023-12-23 01:47:21 +0530

    Time: 39 ms (84.74%), Space: 12.5 MB (8.41%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -14,6 +14,36 @@ public:
         return dp[i][j] = minCost;
     }
     
+    int tabulation(vector<int> &cuts, int m) {
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+        
+        // base case 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i > j) dp[i][j] = 0;
+            }
+        }
+        
+        // tabulation -> reverse the order
+        // i will go from m-2 -> 1
+        // j will go from i -> m-2
+        for (int i = m-2; i >= 1; i--) {
+            for (int j = i; j < m-1; j++) {
+                int minCost = INT_MAX;
+                for (int k = i; k <= j; k++) {
+                    // when i == 1 and j == m-1
+                    // then cost will be n - 0 => n -> use same logic for other values of i & j
+                    minCost = min(minCost, 
+                             cuts[j+1] - cuts[i-1] 
+                                + dp[i][k-1] 
+                                  + dp[k+1][j]);
+                }
+                dp[i][j] = minCost;
+            }
+        }
+        return dp[1][m-2];
+    }
+    
     int minCost(int n, vector<int>& cuts) {
         // its partitioning problem -> here we have to find the optimal partioning of the rod
         // based on the cuts array
@@ -24,6 +54,7 @@ public:
         vector<vector<int>> dp(m, vector<int>(m, -1));
         // we are sorting so that when we partion cuts array, the 2 partitions are independent
         sort(cuts.begin(), cuts.end());
-        return solve(1, m-2, cuts, dp);
+        // return solve(1, m-2, cuts, dp);
+        return tabulation(cuts, m);
     }
 };
