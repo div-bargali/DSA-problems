ea618bb950b7238262fcc105f741c0034fe9febb
Divyanshu Bargali <divyanshubargali@gmail.com>
2023-12-25 19:55:50 +0530

    Time: 157 ms (67.92%), Space: 9.3 MB (49.17%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -29,11 +29,28 @@ public:
         // return solve(0, n, s, dp) - 1; // since we are counting 1 extra partion in the end
         // tabulation 
         // base case
+        
+        // Optimization to reduce time complexity to O(N*N)
+        vector<vector<bool>> palindrome(n+1, vector<bool>(n+1, false));
+            // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++) {
+            palindrome[i][i] = true;
+        }
+
+        // Check substrings of length 2 or more
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                // Check if the substring (i, j) is a palindrome
+                palindrome[i][j] = (s[i] == s[j]) && (len == 2 || palindrome[i + 1][j - 1]);
+            }
+        }
+        
         dp[n] = 0;
         for (int i = n-1; i >= 0; i--) {
             int minCuts = INT_MAX;
             for (int k = n-1; k >= i; k--) {
-                if(isPalindrome(s, i, k)) {
+                if(palindrome[i][k]) {
                     minCuts = min(minCuts, 1 + dp[k+1]);
                 }
             }
