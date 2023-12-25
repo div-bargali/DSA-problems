003e3e5558328ac617f1b11ba40a8bd1c025fd4c
Divyanshu Bargali <divyanshubargali@gmail.com>
2023-12-25 20:37:25 +0530

    Time: 215 ms (8.18%), Space: 8.9 MB (38.02%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -17,9 +17,29 @@ public:
         return dp[i][j] = maxSum;
     }
     
+    int solveOptimized(int i, int n, int k, vector<int> &arr, vector<int> &dp) {
+          // base case 
+        if (i == n) return 0; // maxSum will be 0
+        
+        if (dp[i] != -1) return dp[i];
+        
+        int maxSum = -1;
+        for (int x = i; x < i+k && x < n; x++) {
+            // partition the array at x
+            // for left subarray maxSum = max(arr[i ... x]) * x-i+1
+            // for right partition recursively find the ans
+            maxSum = max(maxSum, *max_element(arr.begin()+i, arr.begin()+x+1)*(x-i+1) + 
+                            solveOptimized(x+1, n, k, arr, dp));
+        }
+        return dp[i] = maxSum;
+    }
+    
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(0, n-1, k, arr, dp);
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<int> dp2(n, -1);
+        // return solve(0, n-1, k, arr, dp); TC -> O(N*N*k)
+        // we don't need j because j will always be n-1
+        return solveOptimized(0, n, k, arr, dp2);
     }
 };
