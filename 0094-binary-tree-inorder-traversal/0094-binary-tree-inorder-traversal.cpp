5f196ec8063d9ebf3eeed838f30c7ba576418535
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-01-13 13:46:35 +0530

    Time: 2 ms (40.68%), Space: 8.6 MB (97.23%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,6 +12,43 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        // Morris Inorder Traversal
+        vector<int> ans;
+        // O(1) space complexity
+        TreeNode* cur = root;
+        while(cur != nullptr) {
+            // check if there is a left subtree
+            if (!cur->left) {
+                // no left subtree, so add cur to ans and move to right subtree
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }
+            else {
+                // go to the right most element of left subtree and connect
+                TreeNode* next = cur->left;
+                while(next->right && next->right != cur) {
+                    // we check second condition because there can already be a connection
+                    next = next->right;
+                }
+                // create the connection if it is not there
+                // if already conenction, then break connection -> means left subtree is done
+                // so add cur to the ans
+                if (next->right == cur) {
+                    // already a connection, so break it
+                    next->right = nullptr;
+                    ans.push_back(cur->val);
+                    cur = cur->right;
+                } else {
+                    // create the connection and traverse to left subtree
+                    next->right = cur;
+                    cur = cur->left;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    vector<int> inorderTraversalIterative(TreeNode* root) {
         stack<TreeNode*> stk;
         TreeNode* cur = root;
         vector<int> ans;
