594af530122e0e1f26a2495a568d08a02be55ce6
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-01-16 02:02:13 +0530

    Time: 405 ms (9.53%), Space: 240.5 MB (8.26%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -13,13 +13,13 @@ class Solution {
 public:
     int ans = 0;
     
-    vector<int> checkBST(TreeNode* root, int upperBound) {
+    vector<int> checkBST(TreeNode* root) {
         if (!root) {
             // {isBST, keySum, largestVal, smallestVal}
             return {1, 0, INT_MIN, INT_MAX}; // leaf node called this
         }
-        auto l = checkBST(root->left, root->val);
-        auto r = checkBST(root->right, upperBound);
+        auto l = checkBST(root->left);
+        auto r = checkBST(root->right);
         
         if (l[0] && r[0]) {
             if (root->val > l[2] && root->val < r[3]) {
@@ -34,7 +34,7 @@ public:
        
     }
     int maxSumBST(TreeNode* root) {
-        auto res = checkBST(root, INT_MAX);
+        auto res = checkBST(root);
         return ans;
     }
 };
