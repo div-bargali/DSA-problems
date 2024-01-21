9d931e2b84a868f6ef26531ab4da75ea40c7651f
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-01-21 20:47:32 +0530

    Time: 8 ms (74.98%), Space: 21.5 MB (15.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,6 +11,22 @@
  */
 class Solution {
 public:
+    pair<int, int> checkBalance(TreeNode* root) {
+        // returns {height of subtree, 0/1 for if tree is balanced}
+        if (!root) return {0, 1};
+        
+        auto lef = checkBalance(root->left);
+        int leftH = lef.first;
+        int leftBal = lef.second;
+        
+        auto rig = checkBalance(root->right);
+        int rightH = rig.first;
+        int rightBal = rig.second;
+        
+        if (abs(leftH - rightH) <= 1 && leftBal && rightBal) 
+            return {1 + max(leftH, rightH), 1}; // subtree is balanced
+        return {1 + max(leftH, rightH), 0}; // subtree is not balanced
+    }
     int height(TreeNode* root) {
         int leftH = 0, rightH = 0;
         if (root->left) leftH = height(root->left);
@@ -27,6 +43,9 @@ public:
         return false;
     }
     bool isBalanced(TreeNode* root) {
-        return solve(root);
+        // return solve(root); O(N^2) approach
+        if (!root) return true;
+        auto ans = checkBalance(root);
+        return ans.second;
     }
 };
