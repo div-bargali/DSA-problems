3d2c413c2dd56c045f5ca5f2aa1d713d275c7c1a
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-01-25 19:54:24 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (5.48%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,6 +12,26 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
+        // constant space solution 
+        // similar to Morris pre-order traversal
+        // here we connect righ-most is left subtree to the root->left and not root
+        TreeNode* cur = root;
+        while(cur) {
+            if (cur->left) {
+                TreeNode* next = cur->left;
+                // go to right-most node
+                while(next->right){
+                    next = next->right;
+                }
+                next->right = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
+            }
+            cur = cur->right;
+        }
+    }
+    
+    void flattenONSpace(TreeNode* root) {
         stack<TreeNode*> stk;
         if (!root) return;
         stk.push(root);
