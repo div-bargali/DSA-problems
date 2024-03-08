207e19f2a1498f9123e03ac1497322d097d0131d
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-03-09 02:02:28 +0530

    Time: 61 ms (78.91%), Space: 27 MB (73.19%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,43 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        int n = nums.size(), j, k, i = 0;
-        while (i < n-1) {
-            j = i+1;
-            k = n-1;
-            int t = -nums[i];
-            // 2-sum problem now
-            while (j < k) {
-                if (nums[j] + nums[k] == t) {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    while(j+1 < n && nums[j+1] == nums[j]) 
-                        j++;
-                    j++;
-                    while(k-1 >= 0 && nums[k-1] == nums[k])
-                        k--;
-                    k--;
-                }
-                else if (nums[j] + nums[k] < t) {
-                    while(j+1 < n && nums[j+1] == nums[j])
-                        j++;
-                    j++;
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end()); // Sorting the array
+
+        std::vector<std::vector<int>> result; // Vector to store the result
+        int n = nums.size();
+        int left, right;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) // Avoid duplicates
+                continue;
+
+            left = i + 1; // Left pointer
+            right = n - 1; // Right pointer
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    // Avoid duplicates
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
                 } else {
-                    while(k-1 >= 0 && nums[k-1] == nums[k])
-                        k--;
-                    k--;
+                    right--;
                 }
             }
-            while(i < n-1 && nums[i+1] == nums[i])
-                i++;
-            i++;
         }
-        return ans;
+        return result;
     }
 };
