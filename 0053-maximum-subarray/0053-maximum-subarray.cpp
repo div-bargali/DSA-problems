ef8b324711722c405661143ebd0284a9fe042f28
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-03-27 02:04:32 +0530

    Time: 89 ms (11.65%), Space: 72.8 MB (6.95%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int i, vector<int> &dp, int &ans) {
+    int tabulation(vector<int> &nums, int i, vector<int> &dp, int &ans) {
         if (i == 0) {
             ans = max(ans, nums[0]);
             return nums[0];
         }
         if (dp[i] != INT_MIN) return dp[i];
         
-        int takeOld = solve(nums, i-1, dp, ans) + nums[i];
+        int takeOld = tabulation(nums, i-1, dp, ans) + nums[i];
         int startNew = nums[i];
         dp[i] = max(takeOld, startNew);
         ans = max(ans, dp[i]);
         return dp[i];
     }
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size(), ans = INT_MIN;
+        int n = nums.size(), ans;
         vector<int> dp(n, INT_MIN);
-        solve(nums, n-1, dp, ans);
+        // tabulation(nums, n-1, dp, ans);
+        dp[0] = nums[0]; ans = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            ans = max(ans, dp[i]);
+        }
         return ans;
     }
 };
