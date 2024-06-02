f5a09a6af0ebba0fd1dd1c40671cc8e211623e81
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-06-02 20:18:09 +0530

    Time: 108 ms (66.67%), Space: 21.9 MB (100.00%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int n = s.length();
+        vector<bool> removed(n, false); // for marking index as removed
+        vector<vector<int>> chars(26); // stores index of occurence of chars
+       
+        
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '*') {
+                // if smallest char is present
+                removed[i] = true;
+                for (int j = 0; j < 26; j++) {
+                    if (chars[j].size() > 0) {
+                        int idx = chars[j].back();
+                        removed[idx] = true;
+                        chars[j].pop_back();
+                        break;
+                    }
+                }
+            } else {
+                chars[s[i] - 'a'].push_back(i);
+            }
+        }
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            if (!removed[i])
+                ans += s[i];
+        }
+        return ans;
+    }
+};
