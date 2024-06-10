e0fad736087c2b2150116b07c9455a75a2b3b7fe
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-06-11 00:29:37 +0530

    Time: 1032 ms (12.50%), Space: 52 MB (12.50%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -29,20 +29,20 @@ public:
     int solveTabulation(vector<int> &nums, int k) {
         int n = nums.size(), LGS = 1;
         // dpp[i][k] will store the length of the LGS that ends at index i and has k unequal pairs
-        vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
+        vector<vector<int>> dpp(n+1, vector<int>(k+1, 0));
         // base case - if k = 0, the dp[0..n-1][0] = 1, we can have atleast 1 element
-        for (int i = 0; i < n; i++) dp[i][0] = 1;
+        for (int i = 0; i < n; i++) dpp[i][0] = 1;
         
         for (int i = 1; i < n; i++) {
             for (int prev = 0; prev < i; prev++) {
                 for (int j = 0; j <= k; j++) {
                     if (nums[i] == nums[prev]) {
-                        dp[i][j] = max(dp[i][j], 1 + dp[prev][j]);
+                        dpp[i][j] = max(dpp[i][j], 1 + dpp[prev][j]);
                     } 
                     else if (j > 0) {
-                        dp[i][j] = max(dp[i][j], 1 + dp[prev][j-1]);
+                        dpp[i][j] = max(dpp[i][j], 1 + dpp[prev][j-1]);
                     }
-                    LGS = max(LGS, dp[i][j]);
+                    LGS = max(LGS, dpp[i][j]);
                 }
             }
         }
@@ -50,7 +50,7 @@ public:
     }
     
     int maximumLength(vector<int>& nums, int k) {
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
         // return solve(0, -1, k, nums);
         
         return solveTabulation(nums, k);
