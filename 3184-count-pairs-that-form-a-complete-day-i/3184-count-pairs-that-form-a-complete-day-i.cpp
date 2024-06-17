727478180f9c9b3f4383a8a069705a9448a389e8
Divyanshu Bargali <divyanshubargali@gmail.com>
2024-06-17 09:49:58 +0530

    Time: 10 ms (25.00%), Space: 24.3 MB (25.00%) - LeetHub

---
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& nums) {
+       unordered_map<int, int> past;
+        int ans = 0;
+        past[nums[0] % 24] = 1;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            int rev = (nums[i] % 24) != 0 ? 24 - (nums[i] % 24): 0;
+            if (past.find(rev) != past.end()) {
+                ans += past[rev];
+            }
+            if (past.find(nums[i] % 24) != past.end()) {
+                past[nums[i] % 24]++;
+            }
+            else {
+                past[nums[i] % 24] = 1;
+            }
+        }
+        return ans; 
+    }
+};
